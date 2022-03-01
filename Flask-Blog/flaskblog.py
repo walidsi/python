79ed3f91162f67d54a01cb586730ef83c430ea4c
walidsi <42148514+walidsi@users.noreply.github.com>
2022-03-01 18:38:43 +0200

    Docker related modifocations

--- Flask-Blog/flaskblog.py
+++ Flask-Blog/flaskblog.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
     """ Returns respone for home page."""
-    return "<h1>Hello, Universe!!</h1>"
+    return "<h1>Hello, Universe!</h1>"
 
 @app.route("/about")
 def about():
