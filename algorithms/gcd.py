57b31c7ae804107983713f7142f203ce2f7bd2b3
walidsi <42148514+walidsi@users.noreply.github.com>
2022-04-28 10:38:40 +0200

    Greatest common denominator functions

--- algorithms/gcd.py
+++ algorithms/gcd.py
@@ -6,12 +6,13 @@ def gcd(a, b):
     else:
         div = b
 
-    while div > 1:
+    while div >= 1:
         if a % div == 0 and b % div == 0:
             return div
         div = div - 1
 
-    return None
+
+print(gcd(11, 7))
 
 
 def EuclidianGCD(a, b):
@@ -26,3 +27,23 @@ def EuclidianGCD(a, b):
 print(EuclidianGCD(10, 20))
 print(EuclidianGCD(3918848, 1653264))
 print(EuclidianGCD(357, 234))
+
+
+def gcd2(m, n):
+    if n == 0:
+        return m
+
+    return gcd2(n, m % n)
+
+
+def gcd_iterative(m, n):
+    while n != 0:
+        r = m % n
+        m = n
+        n = r
+
+    return m
+
+
+print(gcd2(60, 24))
+print(gcd_iterative(3918848, 1653264))
