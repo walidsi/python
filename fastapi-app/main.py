6cc278faaed33aea25b6e9b100dea4f0bcd3bf8e
walidsi <42148514+walidsi@users.noreply.github.com>
2022-09-18 04:08:28 +0200

    fix: Use single quotes for strings

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -12,33 +12,33 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
 def root():
-    return {"data": "blog list"}
+    return {'data': 'blog list'}
 
-@app.get("/blog")
+@app.get('/blog')
 def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
     if published:
-        return {"data": f"{limit} published blogs from db"}
+        return {'data': f'{limit} published blogs from db'}
     else:
-        return {"data": f"{limit} blogs from db"}
+        return {'data': f'{limit} blogs from db'}
 
-@app.get("/blog/unpublished")
+@app.get('/blog/unpublished')
 def unpublished():
     return {'data': 'all unpublished blogs'}
 
-@app.get("/blog/{id}")
+@app.get('/blog/{id}')
 def show(id: int):
-    return {"data": f"blog {id}"}
+    return {'data': f'blog {id}'}
 
-@app.get("/blog/{id}/comments")
+@app.get('/blog/{id}/comments')
 def comments(id: int, limit=10):
-    return {"data": f'blog {id}',
-            "comments": [i for i in range(limit)]}    
+    return {'data': f'blog {id}',
+            'comments': [i for i in range(limit)]}    
 
-@app.get("/about")
+@app.get('/about')
 def about():
-    return {"Author": "Walid Ismail"}
+    return {'Author': 'Walid Ismail'}
     
 
 # to copy updates of this file to a running container, use the following command:
