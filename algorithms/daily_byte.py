5abea222a692f32887becceb90ec0291a2cdcd45
walidsi <42148514+walidsi@users.noreply.github.com>
2022-11-11 16:58:21 +0200

    feat: Daily Byte daily coding problem

---
+++ algorithms/daily_byte.py
@@ -0,0 +1,20 @@
+def gb_reverse_string(input: str) -> str:
+    # A string is an immutable object is python, so how best can I do this task??
+    
+    reverse: str = ''
+    for i in range(-1, -len(input)-1, -1):
+       reverse = reverse + input[i]
+       
+    return reverse
+        
+def gb_reverse_string_2(input: str) -> str:
+    reverse: str = ''
+    
+    for c in input:
+        reverse = c + reverse  # Key here is to add reverse to c, not the opposite. This will reverse the string. Nice trick!
+        
+    return reverse
+    
+
+print(gb_reverse_string_2('Hello World'))
+
