068983c3b9179e7a002ea9380ce42f9dbce81e8a
walidsi <42148514+walidsi@users.noreply.github.com>
2022-11-11 20:05:22 +0200

    style: Formatting changes

--- algorithms/daily_byte.py
+++ algorithms/daily_byte.py
@@ -1,27 +1,31 @@
 import timeit
 
+
 def gb_reverse_string(input: str) -> str:
     # A string is an immutable object is python, so how best can I do this task??
-    
     reverse: str = ''
     for i in range(-1, -len(input)-1, -1):
-       reverse = reverse + input[i]
-       
+        reverse = reverse + input[i]
+
     return reverse
-        
+
+
 def gb_reverse_string_2(input: str) -> str:
     reverse: str = ''
-    
+
     for c in input:
-        reverse = c + reverse  # Key here is to add reverse to c, not the opposite. This will reverse the string. Nice trick!
-        
+        # Key here is to add reverse to c, not the opposite. This will reverse the string. Nice trick!
+        reverse = c + reverse
+
     return reverse
-   
+
+
 def gb_reverse_string_3(input: str) -> str:
     return input[::-1]  # Use slicing syntax. Fastest way by far!
 
-print(timeit.timeit('gb_reverse_string("Hello World")', globals=globals()))
-print(timeit.timeit('gb_reverse_string_2("Hello World")', globals=globals()))
-print(timeit.timeit('gb_reverse_string_3("Hello World")', globals=globals()))
+
+print(timeit.timeit('gb_reverse_string("Hello World 2022!")', globals=globals()))
+print(timeit.timeit('gb_reverse_string_2("Hello World 2022!")', globals=globals()))
+print(timeit.timeit('gb_reverse_string_3("Hello World 2022!")', globals=globals()))
 
 print(gb_reverse_string_2('Hello World'))
