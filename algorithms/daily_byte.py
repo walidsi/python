2c041eec9e1e4d9bb861ce0eb834a51e643c8b9c
walidsi <42148514+walidsi@users.noreply.github.com>
2022-11-11 18:36:19 +0200

    feat: Use timeit to measure execution time

--- algorithms/daily_byte.py
+++ algorithms/daily_byte.py
@@ -1,3 +1,5 @@
+import timeit
+
 def gb_reverse_string(input: str) -> str:
     # A string is an immutable object is python, so how best can I do this task??
     
@@ -14,7 +16,12 @@ def gb_reverse_string_2(input: str) -> str:
         reverse = c + reverse  # Key here is to add reverse to c, not the opposite. This will reverse the string. Nice trick!
         
     return reverse
-    
+   
+def gb_reverse_string_3(input: str) -> str:
+    return input[::-1]  # Use slicing syntax. Fastest way by far!
+
+print(timeit.timeit('gb_reverse_string("Hello World")', globals=globals()))
+print(timeit.timeit('gb_reverse_string_2("Hello World")', globals=globals()))
+print(timeit.timeit('gb_reverse_string_3("Hello World")', globals=globals()))
 
 print(gb_reverse_string_2('Hello World'))
-
