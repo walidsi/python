2444f8dd290792f61b63d47bf79995ac29ff4818
walidsi <42148514+walidsi@users.noreply.github.com>
2022-11-12 19:58:00 +0200

    feat: Initial commit

---
+++ algorithms/grind_leetcode.py
@@ -0,0 +1,60 @@
+from typing import List
+import timeit
+
+
+def two_sum(nums: List[int], target: int) -> List[int]:
+    for index in range(len(nums)):  # Order O(n^2)
+        for index2 in range(index + 1, len(nums)):
+            if nums[index] + nums[index2] == target:
+                return [index, index2]
+
+
+def two_sum2(nums: List[int], target: int) -> List[int]:
+    val_idx = {}
+
+    # Order O(n) by using a hash table ro store looped over integers, clever!!
+    for i, n in enumerate(nums):
+
+        diff = target - n
+
+        if diff in val_idx:
+            return [val_idx[diff], i]
+        else:
+            val_idx[n] = i
+
+
+assert (two_sum2([3, 2, 4], 6) == [1, 2])
+
+print(timeit.timeit('two_sum([3, 12, 44, 11, 6, 3, 22, 11, 33, 44, 55, 66, 1, 4], 5)', globals=globals()))
+print(timeit.timeit('two_sum2([3, 12, 44, 11, 6, 3, 22, 11, 33, 44, 55, 66, 1, 4], 5)', globals=globals()))
+
+
+def isValid(s: str) -> bool:
+    if (len(s) % 2 != 0):
+        return False
+
+    stack = list()
+
+    stack.append(s[0])
+
+    for c in s[1:]:
+        stack.append(c)
+
+        if len(stack) >= 2:
+            if (stack[-1] == ']' and stack[-2] == '[') or \
+                (stack[-1] == '}' and stack[-2] == '{') or \
+                    (stack[-1] == ')' and stack[-2] == '('):
+                stack.pop()
+                stack.pop()
+
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
+
+assert (isValid('[](){}') == True)
+assert (isValid('[()]{}') == True)
+assert (isValid('[(]{}]') == False)
+assert (isValid('()') == True)
+assert (isValid("(){}}{") == False)
